e9d8a6faec8a594e9c11da2ea70305f0feb4dca0
dunky-star <dunkygeoffrey39@gmail.com>
2023-12-11 14:28:24 -0600

    Building the model

--- intelligent-bot.py
+++ intelligent-bot.py
@@ -174,6 +174,20 @@ for length in range(1, 25 + 1):
 
 # Creating placeholders for the inputs and the targets
 
+def model_inputs():
+    inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
+    targets = tf.placeholder(tf.int32, [None, None], name = 'target')
+    lr = tf.placeholder(tf.float32, name = 'learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+    return inputs, targets, lr, keep_prob
 
 
-    
+# Preprocessing the targets
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
+
+
+
