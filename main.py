5b7289e6ac53051a32023c3df358c1c8af4e4ddd
dunky-star <dunkygeoffrey39@gmail.com>
2023-12-15 08:40:42 -0600

    While loop pending implementation

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Implement a while loop that uses case:
+
+while True:
+    case chat:
+        // Execute customer-service-chat
+    case report:
+        // Execute report-generation-agent
+    case q or quit:
+        // Execute exit program
+    
+'''
